e2761910974f63db7a9ea1ea854ca282fe9efe03
Daniel Souza <danielfsouza@gmail.com>
2021-06-03 16:17:35 -0300

    Aula JPA com maven e persistência com MySQl

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,38 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+//		Pessoa p1 = new Pessoa(null,"Carlos da Silva","carlos@gmail.com");
+//		Pessoa p2 = new Pessoa(null,"Joaquim Torres","joaquim@gmail.com");
+//		Pessoa p3 = new Pessoa(null,"Ana Maria","ana@gmail.com");em.
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+		
+//		Pessoa p = em.find(Pessoa.class, 1);
+//		System.out.println(p);
+		
+		em.getTransaction().begin();
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		emf.close();
+		em.close();
+	}	
+
+}
